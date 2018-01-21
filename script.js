7db2db553fd87d53dc205998e2bdf28be17849f4
Greymouth <thejadenjack@gmail.com>
2018-01-20 17:56:23 -0700

    About me and fullpage update

--- script.js
+++
@@ -1,9 +0,0 @@
-function resize() {
-	var main = document.getElementById('main');
-	
-	main.style.width = window.innerWidth + 'px';
-	main.style.height = window.innerHeight + 'px';
-};
-
-window.onload = resize;
-window.onresize = resize;
--- tailwind.js
+++ tailwind.js
@@ -43,7 +43,7 @@ let colors = {
 	'black': '#22292f',
 	'grey-darkest': '#3d4852',
 	'grey-darker': '#606f7b',
-	'grey-dark': '#8795a1',
+	'grey-dark': '#3D3A40',
 	'grey': '#CBC0D3',
 	'grey-light': '#dae1e7',
 	'grey-lighter': '#f1f5f8',
